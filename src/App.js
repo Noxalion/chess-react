5a31268c46e00d1f57d3b4fa4677c6ecea6a61e5
Mac administrator <admin@Macs-MacBook-Pro.local>
2023-11-14 11:49:01 +0100

    set up the pieces

--- src/App.js
+++ src/App.js
@@ -1,6 +1,55 @@
 import './App.css';
 
-function squares() {
+function piece(row, column, maxRows, team) {
+  let type = null;
+  let letter;
+
+  if (row == 1 || row == maxRows - 2) {
+    type = "pawn";
+    letter = "P";
+  }else if (row == 0 || row == maxRows - 1) {
+    if (column == 0 || column == 7) {
+      type = "castle";
+      letter = "C";
+    }else if (column == 1 || column == 6) {
+      type = "horse";
+      letter = "H";
+    }else if (column == 2 || column == 5) {
+      type = "bishop";
+      letter = "B";
+    }else if (column == 3) {
+      type = "queen";
+      letter = "Q";
+    }else{
+      type = "king";
+      letter = "K";
+    }
+  }
+  
+  if (type !== null) {
+    return(<li className={`piece ${team} ${type} c${row}-${column}`} key={letter + row + "-" + column} >{letter}</li>);
+  }
+}
+
+
+
+function Pieces() {
+  let pieces = [];
+  let team = "white";
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      if (i >= 4) {
+        team = "black";
+      }
+      pieces.push(piece(i, j, 8, team));
+    }
+  }
+  return pieces;
+}
+
+
+
+function Squares() {
   var squares = [];
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
@@ -22,11 +71,18 @@ function squares() {
   return squares;
 }
 
+
+
 function Board() {
   return (
-    <ul className="board">
-      {squares()}
-    </ul>
+    <div className='game'>
+      <ul className="game__el board">
+        {Squares()}
+      </ul>
+      <ul className="game__el pieces">
+        {Pieces()}
+      </ul>
+    </div>
   );
 }
 
